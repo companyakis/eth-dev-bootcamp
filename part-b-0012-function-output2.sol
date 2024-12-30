5ed8f34a70d3c912d46cea86bc0296767d98dcae
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-30 22:16:47 +0300

    Create part-b-0012-function-output2.sol

---
+++ part-b-0012-function-output2.sol
@@ -0,0 +1,8 @@
+// Complete the code below to calculate the square of variable x. Assign the result to the implicitly returned variable result.
+
+
+function calculateSquare(int x) returns (int result) {
+
+    result = x * x;
+
+}
