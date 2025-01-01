71f473803f329a1e28951d6a2aaf45694efac8f9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-01 20:02:18 +0300

    Create part-b-0022-state-variable.sol

---
+++ part-b-0022-state-variable.sol
@@ -0,0 +1,16 @@
+// Create a state variable of type uint named amount with a value of 22.
+
+
+contract Variables {
+
+  uint amount = 22;
+
+  function doSomething() public {
+
+    uint i = 456;
+  }
+
+}
+
+
+// A state variable in Solidity is a persistent data storage location within a smart contract that maintains its value across multiple function calls and transactions.
