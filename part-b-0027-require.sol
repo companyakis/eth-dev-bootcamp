e9b4dc34c82e74885f76f37a0cabe7f3d8d427ff
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-01 20:48:35 +0300

    Create part-b-0027-require.sol

---
+++ part-b-0027-require.sol
@@ -0,0 +1,14 @@
+// Write a require statement that ensures that a given variable x is greater than 10 with no message.
+
+require(x > 10);
+
+
+
+
+
+
+// require checks if a certain condition is satisfied in the code.
+
+// If not, then it reports an error message and terminates the execution. The error message is optional.
+
+// require(recipient != address(0), "Recipient address cannot be zero");
