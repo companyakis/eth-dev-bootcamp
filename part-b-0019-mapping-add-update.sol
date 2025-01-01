2c2df3629f2c1798e7ab50a2ad52aeaa0b58472a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-01 19:50:15 +0300

    Create part-b-0019-mapping-add-update.sol

---
+++ part-b-0019-mapping-add-update.sol
@@ -0,0 +1,12 @@
+mapping(address => uint256) public balance;
+
+balance[address(0x123)] = 10; // add
+
+balance[address(0x123)] = 20; // update
+
+
+
+
+
+// For mappings, we could have adding, updating, deleting, and querying
+
