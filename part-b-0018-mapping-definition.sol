0959b9b31e836e030f4022b9a4a3c003ed27393e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-01 19:45:04 +0300

    Create part-b-0018-mapping-definition.sol

---
+++ part-b-0018-mapping-definition.sol
@@ -0,0 +1,11 @@
+// Create a mapping that has a key of type address and a value of type uint named pool.
+
+mapping(address => uint) pool;
+
+
+
+
+
+
+
+// mapping(address => mapping(uint256 => uint256)) public nestedMapping;
