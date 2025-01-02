1bb5036222abd2425c53be99cec95ff38ab63797
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-02 18:51:39 +0300

    Delete part-b-0007-bool2.sol

--- part-b-0007-bool2.sol
+++
@@ -1,9 +0,0 @@
-// Given two booleans named d and e , create a bool named example to determine if either would return true.
-
-pragma solidity ^0.8.7;
-
-contract Example {
-
-  bool example = d || e;
-
-}
