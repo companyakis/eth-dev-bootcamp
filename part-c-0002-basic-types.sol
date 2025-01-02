1e0280cd02150fb66640a96e4f4a9169788e952f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-02 19:40:23 +0300

    Create part-c-0002-basic-types.sol

---
+++ part-c-0002-basic-types.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract FinTechMustafa2025 {
+
+    // some examples
+
+    bool isCompleted = false;
+
+    bool public iAmHere = true;
+
+    uint16 thisYear = 2025;
+
+    int256 ABigNegative = -14598712;
+
+    int32 aPositiveNumber = 444444;
+
+    uint8 companAge = 35;
+
+    string founder = "Mustafa Buyukdereli";
+
+    bytes32 whoAmI = "Mustafa Buyukdereli";
+
+    address public whoseAddress = 0xAB1b7206aa6840C795aB7A6AE8b15417B7E63A8D;
+
+}
