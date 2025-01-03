ab1807b04d4adaf692b54d97299999e0ae247982
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-03 19:26:51 +0300

    Create part-c-0005-address.sol

---
+++ part-c-0005-address.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract FinTech2025 {
+
+    // Unassigned variable
+
+    address public defaultAddr; // default value => 0x0000000000000000000000000000000000000000
+
+    address aRandomAddress = 0x388C818CA8B9251b393131C08a736A67ccB19297;
+
+}
