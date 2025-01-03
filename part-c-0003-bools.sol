391265fb9ec9da73155b6a8578c2dc8dd3575f2e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-03 19:19:33 +0300

    Create part-c-0003-bools.sol

---
+++ part-c-0003-bools.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract FinTech2025 {
+
+    bool public x = true;
+
+    bool public y = false;
+
+    bool public r1 = x != y;
+
+    bool public r2 = x ==y;
+
+    bool public r3 = x && y;
+
+    bool public r4 = x || y;
+
+}
