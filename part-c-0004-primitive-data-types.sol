fa8dcbbe898ce1c7041f5fd882bdde0b6e860277
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-03 22:42:01 +0300

    Create part-c-0004-primitive-data-types.sol

---
+++ part-c-0004-primitive-data-types.sol
@@ -0,0 +1,19 @@
+/*
+Assignment
+Create a new variable newAddr that is a public address and give it a value that is not the same as the available variable addr.
+Create a public variable called neg that is a negative number, decide upon the type.
+Create a new variable, newU that has the smallest uint size type and the smallest uint value and is public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Primitives {
+
+    address public newAddr = 0x95c2faF4339175C902113ae01DDFf09c125c7754;
+
+    int16 public neg = -19000;
+
+    uint8 public newU = 0;
+
+}
