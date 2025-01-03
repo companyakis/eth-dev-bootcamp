018982f0d3cb4c447e9ab7c6188426277d8d6c3b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-03 22:46:59 +0300

    Create part-c-0005-variables.sol

---
+++ part-c-0005-variables.sol
@@ -0,0 +1,19 @@
+/*
+Assignment
+Create a new public state variable called blockNumber.
+Inside the function doSomething(), assign the value of the current block number to the state variable blockNumber.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Variables {
+
+    uint256 public blockNumber;
+
+    function doSomething() public {
+
+        blockNumber = block.number;
+    }
+
+}
